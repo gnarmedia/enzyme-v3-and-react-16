d2e770c52da9013ae4077feadf2e0272b67885bf
gnarmedia <gnarmedia@users.noreply.github.com>
2018-09-13 20:52:51 -0700

    rewire

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file / route
- */
-
-// temporary redirect to Sign Up until compelte
-import SignUp from './sign-up'
-
-export default SignUp
--- src/pages/sign-up.js
+++
@@ -1,7 +0,0 @@
-/**
- * @file /sign-up route
- */
-
-import SignUp from '../components/SignUp/SignUp'
-
-export default SignUp
